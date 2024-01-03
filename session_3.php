3204cf90afb9d4373a1970fc11c6042fd89a8066
Tasneemsherif <74153833+Tasneemsherif@users.noreply.github.com>
2024-01-03 15:29:45 +0200

    Add files via upload

---
+++ session_3.php
@@ -0,0 +1,14 @@
+<?php
+$age = 23;
+$name = "Tasneem";
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+    <h1>
+        <?php
+        echo "My name is $name and I am $age years old.";
+        ?>
+    </h1>
+</html>
