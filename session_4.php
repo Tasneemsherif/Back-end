439196d38eefb0adeb624c979db08f6cab1f19e3
Tasneemsherif <74153833+Tasneemsherif@users.noreply.github.com>
2024-01-09 17:25:49 +0200

    Add files via upload

---
+++ session_4.php
@@ -0,0 +1,14 @@
+<!document html>
+<html>
+    <head>
+        <title>session 4</title>
+    </head>
+    <body>
+        <h1>
+        <?php
+            $date_time = date("d/m/Y - H:i:s");
+            echo "Hello, today is $date_time";
+        ?>
+        </h1>
+    </body>
+</html>
